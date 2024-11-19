7d1cb9325bec92edbaed2a48098a1ab34d0364c2
aakash <aakashsehgal01@gmail.com>
2024-11-19 19:26:27 +0000

    after setting webhook

--- src/main.java
+++ src/main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Jenkins! I made this change to trigger pipeline");
+        System.out.println("Hello, Jenkins! I made this change to trigger pipeline after installing ngrok.");
     }
 }
 
