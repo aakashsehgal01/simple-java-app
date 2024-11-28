7a44e6bf4d599d9a5fce27e5ff27befad11e3b4d
aakash <aakashsehgal01@gmail.com>
2024-11-28 19:31:31 +0000

    ok

--- src/main.java
+++ src/main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Jenkins! I made this change to trigger pipeline after installing ngrok.After hell lot of attempt.Ok after running manually");
+        System.out.println("Hello, Jenkins! I made this change to trigger pipeline after installing ngrok.After hell lot of attempt.Ok after running manually!");
     }
 }
 
