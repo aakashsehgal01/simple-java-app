062315629aa2f68d29eba1c3071b402cc1faf5c3
Aakash Sehgal <w1875948@my.westminster.ac.uk>
2024-12-08 15:13:24 +0000

    2nd change in main.java

--- src/main.java
+++ src/main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Jenkins! i made changes in my local. Learning with AWS");
+        System.out.println("Added another temp path for jenkins built node to work. Learning with AWS");
     }
 }
 
